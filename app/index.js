f87826c7a10dd2faf72772ef1911e5e4973e80d8
DSRoden <Daniel.S.Roden@gmail.com>
2014-07-30 15:37:55 -0500

    sum and sumlist

--- app/index.js
+++ app/index.js
@@ -14,8 +14,33 @@ app.get('/', function(req,res){
   res.render('home.ejs');
 });
 
-app.get('/checkers.ejs', function(req,res){
-  res.render('checkers.ejs');
+app.get('/checkers', function(req,res){
+  res.render('checkers');
+});
+
+app.get('/add/:x/:y/:z/:a', function(req,res){
+  req.params.x *=1;
+  req.params.y *=1;
+  req.params.z *=1;
+  req.params.a *=1;
+
+  console.log(req.params, req.query);
+  req.params.fontsize =req.query.fontsize;
+  req.params.color =req.query.color;
+  req.params.borderwidth=req.query.borderwidth;
+
+  res.render('sum', req.params);
+});
+
+
+app.get('/sumlist/:string', function(req,res){
+  
+  req.params.string;
+
+  req.params.evenC= req.query.even;
+  req.params.oddC = req.query.odd;
+  
+  res.render('sumlist', req.params);
 });
 
 app.listen(process.env.PORT, function(){
